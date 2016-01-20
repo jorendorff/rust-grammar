4f14cbf9fee16a98639db79f005958770a4b8501
Jason Orendorff <jason.orendorff@gmail.com>
2016-01-20 13:37:21 -0600

    Add a little more testing code to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 ANTLR4_DOWNLOAD_DIR="$PWD"
 ## wget -N http://www.antlr.org/download/antlr-4.5.1-complete.jar
-export CLASSPATH="${ANTLR4_DOWNLOAD_DIR}/antlr-4.5.1-complete.jar"
+export CLASSPATH=".:${ANTLR4_DOWNLOAD_DIR}/antlr-4.5.1-complete.jar"
 
 function antlr4 {
     java -jar "${ANTLR4_DOWNLOAD_DIR}/antlr-4.5.1-complete.jar" "$@"
 }
 
 function grun {
-    CLASSPATH=".:$CLASSPATH" java org.antlr.v4.gui.TestRig "$@"
+    java org.antlr.v4.gui.TestRig "$@"
+}
+
+function gbuild {
+    ./build.sh
+}
+
+function gcheck {
+    find ../rust/src/libsyntax -name '*.rs' | java ParserDriver
 }
